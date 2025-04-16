58fa9320a1e191fe4b21b97b78800e4be8685e37
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2025-04-16 16:59:24 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,36 @@
+import requests
+import pandas as pd
+
+Companies = ["IBM","AAPL"]
+
+
+for Compnay in Companies:
+    # API URL
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={Compnay}&interval=5min&apikey=O6DMM9OMXYOG6E9V&#39;"
+
+    # Get the data
+    r = requests.get(url)
+    data = r.json()
+    print(data)
+
+    # Extract the time series data
+    time_series = data["Time Series (5min)"]
+    time_series
+
+    # Convert to DataFrame
+    df = pd.DataFrame.from_dict(time_series, orient='index')
+
+    # Optional: Rename columns for better readability
+    df.columns = ["Open", "High", "Low", "Close", "Volume"]
+
+    # Convert the index to datetime format
+    df.index = pd.to_datetime(df.index)
+
+    # Sort the data by datetime (ascending)
+    df = df.sort_index()
+
+    # Display the DataFrame
+    # print(df)
+
+
+    df.to_csv(f"./Data/{Compnay}_STOCK_DATA.csv",index=False)
